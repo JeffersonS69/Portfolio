0b17a626cc24169d7fb5d12a92dc58e22ea3825c
JeffersonS69 <ibarra.jefferson1@gmail.com>
2025-04-27 23:37:16 -0500

    Implement code changes to enhance functionality and improve performance

--- src/components/navegation/burger.tsx
+++ src/components/navegation/burger.tsx
@@ -29,25 +29,34 @@ const Burger = () => {
           <SheetTitle>
             <div className="flex items-center gap-2">
               <img
-                src="/js.svg"
-                className="max-h-8 bg-amber-50 rounded-md border-2"
-                alt="js.svg logo"
+                src="./Jeffry.png"
+                className="max-h-12 drop-shadow-xl bg-gray-400/80 rounded-lg border-1 border-gray-500"
+                alt="jeffry.png logo"
               />
-              <span className="text-lg font-semibold tracking-tighter">
-                Jefferson
-              </span>
+              <div>
+                <p className="text-sm text-gray-600 tracking-wide  dark:text-gray-400">
+                  Jefferson Garcia
+                </p>
+                <p className="text-sm text-gray-400 tracking-wide  dark:text-gray-600">
+                  ibarra.jefferson1@gmail.com
+                </p>
+              </div>
             </div>
           </SheetTitle>
         </SheetHeader>
-        <div className="flex flex-col gap-6 pb-4">
+        <div className="flex flex-col gap-5 pb-6">
           {MENU.map((item) => (
-            <a href={item.link} key={item.name}>
-              <SheetTrigger asChild className="text-lg">
-                <Button variant="link" className="cursor-pointer">
+            <SheetTrigger asChild key={item.name}>
+              <a href={item.link}>
+                <Button
+                  type="submit"
+                  variant="link"
+                  className="cursor-pointer text-lg w-full items-start justify-start"
+                >
                   {item.name}
                 </Button>
-              </SheetTrigger>
-            </a>
+              </a>
+            </SheetTrigger>
           ))}
         </div>
       </SheetContent>
